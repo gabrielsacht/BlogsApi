46d8d61bab197eb82429cb947fda82bc2940bf36
gabrielsacht <gabrielsachtr@gmail.com>
2022-10-26 16:30:05 -0300

    requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  });
+
+  // User.associate = (models) => {
+  //   User.hasOne(models.Address,
+  //     { foreignKey: 'userID', as: 'blog_posts' });
+  // };
+
+  return User;
+};
